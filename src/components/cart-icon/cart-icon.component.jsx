c5ed6211b63324d8873ebf9b3468ba9abbd5a2d9
kareem ali <kareem.gaad@gmail.com>
2020-07-10 14:23:22 +0200

    2nd commit

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -13,6 +13,7 @@ const CartIcon= ({toggleCartHidden, itemCount})=> (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
         <span className='item-count'>{itemCount}</span>
+        {/* <div></div> */}
     </div>
 );
 const mapDispatchToProps = dispatch =>({
